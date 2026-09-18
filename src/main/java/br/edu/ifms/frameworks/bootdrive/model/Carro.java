6bd51ce2058688c368cff25c684dbfaf7c4855b0
Roosevelt Silva <roosilva@gmail.com>
2026-09-18 08:53:23 -0400

    Aula 01

---
+++ src/main/java/br/edu/ifms/frameworks/bootdrive/model/Carro.java
@@ -0,0 +1,23 @@
+package br.edu.ifms.frameworks.bootdrive.model;
+
+public class Carro {
+    private Long id;
+    private String placa;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
